62f7a9f2568065514dcfe543abe8160a3c7cd567
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-01-24 01:06:40 +0000

    Auto-generated commit 24/01/2015 1:06:35 AM

--- booking.php
+++ booking.php
@@ -67,7 +67,7 @@ get_header(); ?>
 										<div class="row">
 											<div class="small-6 medium-6 large-6 columns">
 												<div class="row collapse">
-													<div class="small-4 medium-4 large-4 columns">
+													<div class="small-2 medium-2 large-2 columns">
 														<span class="prefix"><span data-icon="&#xf015" aria-hidden="true"></span></span>
 													</div>
 													<div class="small-10 medium-10 large-10 columns">
